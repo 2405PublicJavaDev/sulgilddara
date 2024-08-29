fd6f85cd23fbb5ffdda980cf53e9fbfa645f863e
kh-jwkim <kh.jwkim@gmail.com>
2024-08-29 15:16:46 +0900

    [INIT] 프로젝트 초기 구성
    spring initializr 설정
    Project: Gradle-Groovy
    Language: Java
    Spring Boot: 3.3.3
    Packaging: Jar
    Java: 17
    
    Dependencies
    - Spring Web
    - Spring Boot Dev Tools
    - Spring Data JDBC
    - MyBatis Framework
    - Oracle Driver
    - Lombok

---
+++ src/main/java/com/makjan/sulgilddara/SulgilddaraApplication.java
@@ -0,0 +1,13 @@
+package com.makjan.sulgilddara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SulgilddaraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SulgilddaraApplication.class, args);
+	}
+
+}
