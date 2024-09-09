a7813c429a4f53c3af53313ea9bd2a55e6157b8f
Ella Eom <eomji1233@gmail.com>
2024-09-09 09:01:57 +0900

    [MODIFY] 사용자 메인페이지, 양조장 리스트 페이지 구현
    1. 양조장 주소, 해시태그, 주종 출력 구현
    2. 지역별 지도 클릭 시 리스트 출력 구현

--- src/main/java/com/makjan/sulgilddara/brewery/common/config/BreweryFileConfig.java
+++ src/main/java/com/makjan/sulgilddara/brewery/common/config/BreweryFileConfig.java
@@ -7,7 +7,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class BreweryFileConfig implements WebMvcConfigurer {
 	private String webPath = "/brewery-images/**";
-	private String realPath = "file:C:/uploadFile/brewery/";
+//	private String realPath = "file:C:/uploadFile/brewery/";
+	private String realPath = "file:/Users/eom-eunji/Downloads/uploadFile/brewery/";
 	
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
